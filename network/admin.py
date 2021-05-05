cfc421a96e7ee8cb70e51574a8d075641c3a8281
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2021-05-05 20:59:05 +0700

    Added User & Post model

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from network.models import User, Post
+
+admin.site.register(User)
+admin.site.register(Post)
--- network/models.py
+++ network/models.py
@@ -3,4 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField("self", related_name='following', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return f'{self.username}'
+
+
+class Post(models.Model):
+    content = models.TextField(max_length=200)
+    publisher = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    published = models.DateTimeField(auto_now=True)
+    like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.content[:5]}... by {self.publisher}'
+
