5b3c6e4a1b2d1c1ce7d891419163d010c84a627b
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2021-05-16 21:07:04 +0700

    Added age and gender fields to User model

--- network/models.py
+++ network/models.py
@@ -3,7 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
+    MALE = 'M'
+    FEMALE = 'F'
+    NOT_GIVEN = 'NG'
+
+    GENDER_CHOICES = [
+        (MALE, 'MALE'),
+        (FEMALE, 'FEMALE'),
+        (NOT_GIVEN, 'NOT_GIVEN')
+    ]
+
     followers = models.ManyToManyField("self", related_name='following', symmetrical=False, blank=True)
+    age = models.SmallIntegerField(help_text="How old are you ?", default=0)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default=NOT_GIVEN)
 
     def __str__(self):
         return f'{self.username}'
