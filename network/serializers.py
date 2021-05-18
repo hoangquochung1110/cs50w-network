86c3ee41e632f4cb108a14739ac6cdffa5a6f7f2
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2021-05-18 13:41:23 +0700

    Defined custom fields on
    ReadUSerSerializer and FollowSerializer

--- network/serializers.py
+++ network/serializers.py
@@ -3,10 +3,30 @@ from rest_framework import serializers
 from network.models import Post, User
 
 
-class ReadUserSerializer(serializers.ModelSerializer):
+class FollowSerializer(serializers.ModelSerializer):
+    """
+    Store public information on a specific user
+    """
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'age', 'gender',]
+        fields = ['username', 'age', 'gender']
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
+    followers = FollowSerializer(many=True)
+    following = FollowSerializer(many=True)
+    number_of_following = serializers.SerializerMethodField()
+    number_of_followers = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'age', 'gender', 'followers', 'number_of_followers', 'following', 'number_of_following', ]
+
+    def get_number_of_followers(self, obj):
+        return obj.followers.count()
+
+    def get_number_of_following(self, obj):
+        return obj.following.count()
 
 
 class ReadPostSerializer(serializers.ModelSerializer):
