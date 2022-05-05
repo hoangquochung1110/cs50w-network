f98b90a857dd157188b0714c13b65d5eaf209815
hunghoang-saritasa <hung.hoang@saritasa.com>
2022-05-05 20:53:13 +0700

    Remove unused vanilla js

--- tasks.py
+++ tasks.py
@@ -1,3 +1,8 @@
+# import csv
+# import os
+# import sqlite3 as sql
+# from sqlite3 import Error
+
 from invoke import Exit, UnexpectedExit, task
 from rich import print
 from rich.panel import Panel
@@ -33,3 +38,28 @@ def checkall(context, path=BASE_FOLDERS):
         msg = f"Linters failed: {', '.join(map(str.capitalize, failed))}"
         print(Panel(msg, style="yellow bold"))
         raise Exit(code=1)
+
+# @task
+# def convert_sqlite3_to_csv_file(context):
+#     try:
+
+#         # Connect to database
+#         conn=sql.connect('db.sqlite3')
+#         # Export data into CSV file
+#         print(Panel(msg="Exporting data into CSV..........."), style="green"))
+#         cursor = conn.cursor()
+#         cursor.execute("select * from Employee")
+#         with open("employee_data.csv", "w") as csv_file:
+#             csv_writer = csv.writer(csv_file, delimiter="\t")
+#             csv_writer.writerow([i[0] for i in cursor.description])
+#             csv_writer.writerows(cursor)
+
+#         dirpath = os.getcwd() + "/employee_data.csv"
+#         # print "Data exported Successfully into {}".format(dirpath)
+
+#     except Error as e:
+#         print(e)
+
+#     # Close database connection
+#     finally:
+#         conn.close()
