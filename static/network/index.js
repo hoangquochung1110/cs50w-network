bf2f1b7334a1754e573aef3d427edabdfd604222
hunghoang-saritasa <hung.hoang@saritasa.com>
2022-05-05 20:52:46 +0700

    Fix navbar offsetHeight

--- static/network/index.js
+++ static/network/index.js
@@ -1,30 +1,19 @@
-import { getPosts, createNewPost } from './components/post.js';
-import { showUserProfilePopup, hideUserProfilePopup } from './components/userprofile.js';
+const nav = document.querySelector('.navbar');
+let topOfNav = nav.offsetTop; // assign to a let to avoid layout thrashing
 
-const FIRSTPAGE = 1;
-const ANONYMOUSUSER = -1;
+window.addEventListener('scroll', () => {
+    const offsetHeight = nav.offsetHeight;
+    fixNav(nav, topOfNav, offsetHeight);
+})
 
-const host_user_id = JSON.parse(document.querySelector("#user_id").textContent);
+const fixNav = (nav, topOfNav, offsetHeight) => {
+    const body = document.querySelector('.body');
+    if(window.scrollY >= topOfNav){
+        body.style.paddingTop = offsetHeight + 'px';
+        nav.classList.add('fixed-nav');
+    } else {
+        body.style.paddingTop = 0;
+        nav.classList.remove('fixed-nav');
 
-const allPostsContainer = document.querySelector('.all-posts');
-const followingPostsContainer = document.querySelector('.following-posts');
-const overlay = document.querySelector('.overlay');
-
-document.addEventListener('DOMContentLoaded', function() {
-
-    if (allPostsContainer){
-        getPosts('/posts/', FIRSTPAGE, allPostsContainer);
-        allPostsContainer.addEventListener('click', showUserProfilePopup);
-    } else if (followingPostsContainer){
-        getPosts('posts/following/', FIRSTPAGE, followingPostsContainer);
-        followingPostsContainer.addEventListener('click', showUserProfilePopup);
-    } else{
-        throw new Error('Can not load page at the moment')
     }
-
-    if (host_user_id > ANONYMOUSUSER){ // user.is_authenticated == True
-        const newPostForm = document.querySelector('#new-post__form');
-        newPostForm.addEventListener('submit', createNewPost);
-    }
-    overlay.addEventListener('click', hideUserProfilePopup);
-});
+}
